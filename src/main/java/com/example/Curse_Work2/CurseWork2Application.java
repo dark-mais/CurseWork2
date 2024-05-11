b4a460e3862198b36aabbb506b4ace8617011a91
dark-mais <dark1mais@gmail.com>
2024-05-11 10:03:32 +0300

    commit

---
+++ src/main/java/com/example/Curse_Work2/CurseWork2Application.java
@@ -0,0 +1,13 @@
+package com.example.Curse_Work2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurseWork2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurseWork2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Curse_Work2/CurseWork2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Curse_Work2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurseWork2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
